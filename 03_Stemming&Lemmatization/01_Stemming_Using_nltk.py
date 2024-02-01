2e2688b821509ac9be60da911964e61c203097e9
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2024-02-01 12:17:01 +0600

    Stemming & Lemmatization

--- 03_Stemming&Lemmatization/01_Stemming_Using_nltk.py
+++ 03_Stemming&Lemmatization/01_Stemming_Using_nltk.py
@@ -5,7 +5,8 @@ from nltk.tokenize import word_tokenize
 stemmer = PorterStemmer()
 
 # Sample text
-text = "Stemming is an important step in natural language processing."
+text = """Natural Language Processing (NLP) is a fascinating field that combines linguistics and computer science to empower machines with language understanding. NLP algorithms parse and analyze human language, enabling applications like chatbots, sentiment analysis, and language translation.Researchers and practitioners in NLP constantly strive to improve models' ability to handle diverse linguistic nuances, context, and ambiguities. Pre-trained language models, such as BERT and GPT, have revolutionized the field, achieving state-of-the-art results in various language-related tasks.Despite advancements, challenges persist, including ethical considerations and biases in NLP models. Creating unbiased and inclusive language models remains a priority for the NLP community to ensure fair and equitable AI applications.Continuous research and innovation drive the evolution of NLP, fostering improved communication between humans and machines. Nicely, Beautifully, additionally, warmly, locally, organizing. cries, crys, cried.
+"""
 
 # Tokenize the text
 words = word_tokenize(text)
