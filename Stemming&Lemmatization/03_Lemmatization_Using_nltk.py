b49bb4cf5ad49f1bc676e35faff2bb67370fa334
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2024-02-01 11:30:16 +0600

    Lemmatization

---
+++ Stemming&Lemmatization/03_Lemmatization_Using_nltk.py
@@ -0,0 +1,34 @@
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+import nltk
+
+# Download the WordNet corpus (needed for lemmatization)
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+# Create a WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+# Sample text
+text = """Natural Language Processing (NLP) is a fascinating field that combines linguistics and computer science to empower machines with language understanding. NLP algorithms parse and analyze human language, enabling applications like chatbots, sentiment analysis, and language translation.
+
+Researchers and practitioners in NLP constantly strive to improve models' ability to handle diverse linguistic nuances, context, and ambiguities. Pre-trained language models, such as BERT and GPT, have revolutionized the field, achieving state-of-the-art results in various language-related tasks.
+
+Despite advancements, challenges persist, including ethical considerations and biases in NLP models. Creating unbiased and inclusive language models remains a priority for the NLP community to ensure fair and equitable AI applications.
+
+Continuous research and innovation drive the evolution of NLP, fostering improved communication between humans and machines. Nicely, Nicely, Nicely, Beautifully, additionally, warmly, locally, organizing.
+"""
+
+# Tokenize the text
+words = word_tokenize(text)
+
+# Create a set of English stopwords
+stop_words = set(stopwords.words('english'))
+words = [word for word in words if word.lower() not in stop_words]
+# Lemmatize the words, excluding stopwords
+lemmatized_words = [lemmatizer.lemmatize(word) for word in words if word.lower() not in stop_words]
+
+# Print the original words and their lemmas
+for original, lemma in zip(words, lemmatized_words):
+    print(f"{original} -> {lemma}")
---
+++ Stemming&Lemmatization/04_Lemmatization_Using_Spacy.py
@@ -0,0 +1,20 @@
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+
+# Load the spaCy English model
+nlp = spacy.load("en_core_web_sm")
+
+# Sample text
+text = """Natural Language Processing (NLP) is a fascinating field that combines linguistics and computer science to empower machines with language understanding. NLP algorithms parse and analyze human language, enabling applications like chatbots, sentiment analysis, and language translation.Researchers and practitioners in NLP constantly strive to improve models' ability to handle diverse linguistic nuances, context, and ambiguities. Pre-trained language models, such as BERT and GPT, have revolutionized the field, achieving state-of-the-art results in various language-related tasks.Despite advancements, challenges persist, including ethical considerations and biases in NLP models. Creating unbiased and inclusive language models remains a priority for the NLP community to ensure fair and equitable AI applications.Continuous research and innovation drive the evolution of NLP, fostering improved communication between humans and machines. Nicely, Beautifully, additionally, warmly, locally, organizing.
+"""
+
+# Process the text with spaCy
+doc = nlp(text)
+words = [token.text for token in doc if token.text.lower() not in STOP_WORDS]
+print("hjkl", words)
+# Get the lemmas excluding stopwords
+lemmas = [token.lemma_ for token in doc if token.lower_ not in STOP_WORDS]
+
+# Print the original words and their lemmas
+for original, lemma in zip(words, lemmas):
+    print(f"{original} -> {lemma}")
